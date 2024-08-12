30f766877a1fd58d5ad48d2ef754e364574f0f3d
Andre Kurait <akurait@amazon.com>
2024-08-12 16:31:33 -0500

    Add max bulk size and requests per second to RFS
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- RFS/src/main/java/com/rfs/common/DocumentReindexer.java
+++ RFS/src/main/java/com/rfs/common/DocumentReindexer.java
@@ -1,5 +1,7 @@
 package com.rfs.common;
 
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -9,33 +11,51 @@ import org.apache.lucene.document.Document;
 import org.opensearch.migrations.reindexer.tracing.IDocumentMigrationContexts;
 
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.BufferOverflowStrategy;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.util.function.Tuple2;
 
 @RequiredArgsConstructor
 public class DocumentReindexer {
     private static final Logger logger = LogManager.getLogger(DocumentReindexer.class);
     protected final OpenSearchClient client;
     private final int numDocsPerBulkRequest;
+    private final long numBytesPerBulkRequest;
     private final int maxConcurrentRequests;
+    private final double maxRequestsPerSecond;
 
     public Mono<Void> reindex(
         String indexName,
         Flux<Document> documentStream,
         IDocumentMigrationContexts.IDocumentReindexContext context
     ) {
-
-        return documentStream.map(this::convertDocumentToBulkSection)  // Convert each Document to part of a bulk
-                                                                       // operation
-            .buffer(numDocsPerBulkRequest) // Collect until you hit the batch size
-            .doOnNext(bulk -> logger.info("{} documents in current bulk request", bulk.size()))
-            .map(this::convertToBulkRequestBody)  // Assemble the bulk request body from the parts
+        final int requestBuffer = Math.min(Math.max((int) maxRequestsPerSecond * 30, 10), 100); // After a pause/slowdown, allow up to 30 seconds of bursting or 100 requests
+        return Flux.interval(Duration.ofMillis((long) (1000 / maxRequestsPerSecond)), Schedulers.single())
+            .onBackpressureBuffer(requestBuffer, BufferOverflowStrategy.DROP_OLDEST)  // Drop ticks on backpressure, note requests are not dropped
+            .zipWith(documentStream
+                .publishOn(Schedulers.single())
+                .map(this::convertDocumentToBulkSection)  // Convert each Document to part of a bulk operation
+                .bufferWhile(bufferPredicate(numDocsPerBulkRequest, numBytesPerBulkRequest)) // Collect until you hit the batch size or max size
+            )
+            .map(Tuple2::getT2)
+            .publishOn(Schedulers.parallel())
             .flatMap(
-                bulkJson -> client.sendBulkRequest(indexName, bulkJson, context.createBulkRequest()) // Send the request
+                bulkSections -> client
+                    .sendBulkRequest(indexName,
+                        this.convertToBulkRequestBody(bulkSections),
+                        context.createBulkRequest()) // Send the request
+                    .doFirst( () ->
+                        logger.info("{} documents in current bulk request. First doc is size {} bytes",
+                            bulkSections.size(),
+                            bulkSections.get(0).getBytes(StandardCharsets.UTF_8).length)
+                    )
                     .doOnSuccess(unused -> logger.debug("Batch succeeded"))
                     .doOnError(error -> logger.error("Batch failed", error))
                     // Prevent the error from stopping the entire stream, retries occurring within sendBulkRequest
-                    .onErrorResume(e -> Mono.empty()),
+                    .onErrorResume(e -> Mono.empty())
+                    .subscribeOn(Schedulers.boundedElastic()),
                 maxConcurrentRequests)
             .doOnComplete(() -> logger.debug("All batches processed"))
             .then();
@@ -55,5 +75,32 @@ public class DocumentReindexer {
             builder.append(section).append("\n");
         }
         return builder.toString();
+    }
+
+    private static java.util.function.Predicate<String> bufferPredicate(int maxItems, long maxSizeInBytes) {
+        return new java.util.function.Predicate<>() {
+            private int currentItemCount = 0;
+            private long currentSize = 0;
+
+            @Override
+            public boolean test(String next) {
+                currentItemCount++;
+                // TODO: Move to Bytebufs to convert from string to bytes only once
+                // Add one for newline between bulk sections
+                currentSize += next.getBytes(StandardCharsets.UTF_8).length + 1;
+
+                // Return true to keep buffering while conditions are met
+                if (currentSize == 0 ||
+                    (currentItemCount <= maxItems && currentSize <= maxSizeInBytes)) {
+                    return true;
+                }
+
+                // Reset and return false to signal to stop buffering.
+                // Next item is excluded from current buffer
+                currentItemCount = 0;
+                currentSize = 0;
+                return false;
+            }
+        };
     }
 }
